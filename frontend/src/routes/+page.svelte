2330289a80e23a7fffa48d0e79641f67a564e8ba
Emre Özcan <emre@emreis.com>
2024-01-14 06:01:38 +0300

    frontend: Add server list to homepage

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import {userStore, serverList} from "$lib/userStore";
+</script>
+
+<style>
+    .content {
+        margin-inline: 1em;
+    }
+</style>
+
+<div class="content">
+    {#if $userStore}
+        <h1>Welcome {$userStore.username} to Miscord!</h1>
+        <p>Here is a list of groups that you're a member of:</p>
+        {#if $serverList}
+            <ul>
+                {#each $serverList as server}
+                    <li>
+                        <a href="/chat/{server.id}">{server.name}</a>
+                    </li>
+                {/each}
+            </ul>
+        {:else}
+            <p>Loading...</p>
+        {/if}
+    {:else}
+        <h1>Miscord</h1>
+
+        <p>
+            You need to have an account to access the marvelous world of Miscord!
+        </p>
+
+        <p>
+            Please create an account <a href="/register">here</a>, or log into your
+            existing account <a href="/login">here</a>!
+        </p>
+    {/if}
+</div>
